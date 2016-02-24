3abec83029ff33625fcdc222d1a5c669e5b532f0
martmwangi <luthermwangi2@gmail.com>
2016-02-24 16:44:14 +0300

    Initial commit

---
+++ app/src/androidTest/java/com/Martmwangi86GmailCom/Demon8Tk/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.Martmwangi86GmailCom.Demon8Tk;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
---
+++ app/src/main/java/com/Martmwangi86GmailCom/Demon8Tk/MyApplication.java
@@ -0,0 +1,19 @@
+package com.Martmwangi86GmailCom.Demon8Tk;
+
+import android.app.Application;
+
+import com.estimote.sdk.EstimoteSDK;
+
+public class MyApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        EstimoteSDK.initialize(getApplicationContext(), "demon-8tk", "f3ed6216ebdf3fd26b6349c0cbc01391");
+
+        // uncomment to enable debug-level logging
+        // it's usually only a good idea when troubleshooting issues with the Estimote SDK
+//        EstimoteSDK.enableDebugLogging(true);
+    }
+}
